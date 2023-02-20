4cb085e440a3f9a5ccceb6980c2fa54f51095780
Lindsey-Harris <119532778+Lindsey-Harris@users.noreply.github.com>
2023-02-20 18:17:12 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,29 @@
-// const { response } = require("express")
-
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
+//calling fetch in client side JS is gonna kick off aschronus operation
 
-}) //calling fetch in client side JS is gonna kick off aschronus IO operation
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=charlotte').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() //going to prevent that default behaviour which is to refresh the browser allowing the server to render any page. Instead its going to allow us to do whatever we want by letting the function run!  
+
+    const location = search.value //only fetch when the form is submitted!
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
